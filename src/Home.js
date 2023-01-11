3622216690afead281b2605320549dc721666b78
Adrian Petrykiw <apetry2@illinois.edu>
2023-01-11 16:39:15 -0600

    initial draft site

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import {
+    useDynamicContext,
+    DynamicWidget,
+  } from "@dynamic-labs/sdk-react";
+  import { useState, useEffect } from "react";
+  import "./Home.css";
+  
+  function Home({ user }) {
+    
+  
+    return (
+      <div className="body">
+
+      </div>
+    );
+  }
+  
+  export default Home;
